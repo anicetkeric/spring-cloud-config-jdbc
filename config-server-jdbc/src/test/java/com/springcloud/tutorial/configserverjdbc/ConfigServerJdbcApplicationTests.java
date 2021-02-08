4093966f3f9874c139bd8299143ddb5c9c5dd412
aek <konan.kouame@soprasteria.com>
2021-02-08 22:35:10 +0100

    server config

---
+++ config-server-jdbc/src/test/java/com/springcloud/tutorial/configserverjdbc/ConfigServerJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.tutorial.configserverjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
